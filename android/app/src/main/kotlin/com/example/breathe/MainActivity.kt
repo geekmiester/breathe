9f3d0dee1530acc996662b3e6355749f3b6f4e02
nohli <43643339+nohli@users.noreply.github.com>
2019-01-28 03:16:30 +0100

    android necessary imports for fullscreen mode

--- android/app/src/main/kotlin/com/example/breathe/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/breathe/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager
+import android.view.View
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
