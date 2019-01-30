060214bb0d87593be6f650f4558722ec833b80c6
nohli <43643339+nohli@users.noreply.github.com>
2019-01-30 01:48:19 +0100

    clean up code, no changes

--- lib/main.dart
+++ lib/main.dart
@@ -23,36 +23,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: pureBlack,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: ''),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -74,29 +56,29 @@ bool run = false;
 
 bool inhale = true;
 
-var inhaleSpeed = inhaleTime / 2.2;
-
-var exhaleSpeed = exhaleTime / 2.2;
-
 var breathCount = 0;
 
 var maxBreathCount = 15;
 
-var maxCircleSize = 0.0;
-
 var minCircleSize = 0.0;
+
+var maxCircleSize = 0.0;
 
 var circleSize = minCircleSize;
 
+var inhaleSpeed = inhaleTime / 2.2;
+
+var exhaleSpeed = exhaleTime / 2.2;
+
 var circle13 = maxCircleSize * 0.13;
 
-var circle20 = maxCircleSize * 0.2;
+var circle20 = maxCircleSize * 0.20;
 
-var circle30 = maxCircleSize * 0.3;
+var circle30 = maxCircleSize * 0.30;
 
-var circle80 = maxCircleSize * 0.8;
+var circle80 = maxCircleSize * 0.80;
 
-var circle90 = maxCircleSize * 0.9;
+var circle90 = maxCircleSize * 0.90;
 
 var circle97 = maxCircleSize * 0.97;
 
@@ -119,9 +101,6 @@ class _MyHomePageState extends State<MyHomePage> {
     while (run) {
       if (inhale) {
         setState(() {
-          // This call to setState tells the Flutter framework that something has
-          // changed in this State, which causes it to rerun the build method below
-          // so that the display can reflect the updated values.
           if (circleSize < circle20)
             circleSize = circleSize + 0.6;
           else if (circleSize < circle30)
@@ -169,12 +148,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     // get the screen dimensions
@@ -223,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
